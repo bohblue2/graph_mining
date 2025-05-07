dc27191b27a3e3a646e3f37dbded6001ccc3b141
kimjisub <0226daniel@gmail.com>
2025-05-07 21:52:41 +0900

    🧑‍💻 Mock data

--- backend/main.py
+++ backend/main.py
@@ -70,56 +70,35 @@ async def get_edges():
 @app.get("/test")
 async def test():
     return {
-    "nodes": [
-        { "id": "반도체",          "type": "theme" },
-        { "id": "2차전지",        "type": "theme" },
-        { "id": "클라우드/AI",    "type": "theme" },
-        { "id": "메타버스",        "type": "theme" },
-        { "id": "헬스케어/바이오", "type": "theme" },
-
-        { "id": "005930", "type": "stock", "code": "005930", "name": "삼성전자" },
-        { "id": "000660", "type": "stock", "code": "000660", "name": "SK하이닉스" },
-        { "id": "000990", "type": "stock", "code": "000990", "name": "DB하이텍" },
-
-        { "id": "051910", "type": "stock", "code": "051910", "name": "LG화학" },
-        { "id": "006400", "type": "stock", "code": "006400", "name": "삼성SDI" },
-        { "id": "096770", "type": "stock", "code": "096770", "name": "SK이노베이션" },
-
-        { "id": "035420", "type": "stock", "code": "035420", "name": "NAVER" },
-        { "id": "035720", "type": "stock", "code": "035720", "name": "카카오" },
-        { "id": "012510", "type": "stock", "code": "012510", "name": "더존비즈온" },
-
-        { "id": "036570", "type": "stock", "code": "036570", "name": "엔씨소프트" },
-        { "id": "112040", "type": "stock", "code": "112040", "name": "위메이드" },
-        { "id": "078340", "type": "stock", "code": "078340", "name": "컴투스" },
-
-        { "id": "068270", "type": "stock", "code": "068270", "name": "셀트리온" },
-        { "id": "207940", "type": "stock", "code": "207940", "name": "삼성바이오로직스" },
-        { "id": "000100", "type": "stock", "code": "000100", "name": "유한양행" }
-    ],
-    "links": [
-        { "source": "반도체",       "target": "005930", "value": 0.88 },
-        { "source": "반도체",       "target": "000660", "value": 0.84 },
-        { "source": "반도체",       "target": "000990", "value": 0.75 },
-
-        { "source": "2차전지",     "target": "051910", "value": 0.92 },
-        { "source": "2차전지",     "target": "006400", "value": 0.89 },
-        { "source": "2차전지",     "target": "096770", "value": 0.80 },
-
-        { "source": "클라우드/AI", "target": "035420", "value": 0.78 },
-        { "source": "클라우드/AI", "target": "035720", "value": 0.82 },
-        { "source": "클라우드/AI", "target": "012510", "value": 0.65 },
-
-        { "source": "메타버스",     "target": "036570", "value": 0.70 },
-        { "source": "메타버스",     "target": "112040", "value": 0.68 },
-        { "source": "메타버스",     "target": "078340", "value": 0.60 },
-
-        { "source": "헬스케어/바이오","target": "068270","value": 0.85 },
-        { "source": "헬스케어/바이오","target": "207940","value": 0.90 },
-        { "source": "헬스케어/바이오","target": "000100","value": 0.72 },
-
-        { "source": "005930","target":"000660","value":0.81,"type":"corr" },
-        { "source": "051910","target":"006400","value":0.88,"type":"corr" },
-        { "source": "068270","target":"207940","value":0.79,"type":"corr" }
-    ]
+        "query_info":{
+            "from": "2024-01-01",
+            "to": "2024-01-02"
+        },
+        "company_nodes":[
+            {
+                "type": "company",
+                "ISIN": "US67066G1040",
+                "name": "NVIDIA",
+                "priceFrom": 113.54,
+                "priceTo": 115.54,
+                "priceChange": 1.0176149375,
+                "currency": "USD",
+                "market": "NASDAQ",
+                "source": "Yahoo Finance",
+            }
+        ],
+        "keyword_nodes":[
+            {
+                "type": "keyword",
+                "keyword": "AI 칩",
+                "priceChange": 1.0176149375,
+            }
+        ],
+        "edges":[
+            {
+                "source": "NVIDIA",
+                "target": "AI 칩",
+                "weight": 0.5,
+            }
+        ]
     }
